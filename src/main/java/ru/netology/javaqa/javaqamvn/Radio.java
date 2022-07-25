cd27c0eaa60cc453655f0df2942c23f3fec796e8
Julia Kostomarova <kostomarova.julia@gmail.com>
2022-07-25 10:48:47 +0400

    added radio files

---
+++ src/main/java/ru/netology/javaqa/javaqamvn/Radio.java
@@ -0,0 +1,63 @@
+package ru.netology.javaqa.javaqamvn;
+
+public class Radio {
+
+    private int currentNumber;
+    private int currentVolume;
+
+    public int getCurrentNumber() {
+        return currentNumber;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentNumber(int currentNumber) {
+        if (currentNumber < 0) {
+            return;
+        }
+        if (currentNumber > 9) {
+            return;
+        }
+        this.currentNumber = currentNumber;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void next() {
+        if (currentNumber < 9) {
+            currentNumber++;
+        } else {
+            currentNumber = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentNumber > 0) {
+            currentNumber--;
+        } else {
+            currentNumber = 9;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/javaqamvn/RadioTest.java
@@ -0,0 +1,132 @@
+package ru.netology.javaqa.javaqamvn;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void increaseNumber() {
+        Radio radio = new Radio();
+
+        radio.setCurrentNumber(9);
+        radio.next();
+
+        int actual = radio.getCurrentNumber();
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void decreaseNumber() {
+        Radio radio = new Radio();
+
+        radio.setCurrentNumber(0);
+        radio.prev();
+
+        int actual = radio.getCurrentNumber();
+        int expected = 9;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void increaseNumber2() {
+        Radio radio = new Radio();
+
+        radio.setCurrentNumber(8);
+        radio.next();
+
+        int actual = radio.getCurrentNumber();
+        int expected = 9;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void decreaseNumber2() {
+        Radio radio = new Radio();
+
+        radio.setCurrentNumber(1);
+        radio.prev();
+
+        int actual = radio.getCurrentNumber();
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void increaseVolume() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(5);
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 6;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void decreaseVolume() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(5);
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 4;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void increaseVolume2() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(10);
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 10;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void decreaseVolume2() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void setNumberLessThanNull() {
+        Radio radio = new Radio();
+
+        radio.setCurrentNumber(-1);
+    }
+
+    @Test
+    public void setNumberMoreThanNine() {
+        Radio radio = new Radio();
+
+        radio.setCurrentNumber(10);
+    }
+
+    @Test
+    public void setVolumeLessThanNull() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(-1);
+    }
+
+    @Test
+    public void setVolumeMoreThanTen() {
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(11);
+    }
+}
